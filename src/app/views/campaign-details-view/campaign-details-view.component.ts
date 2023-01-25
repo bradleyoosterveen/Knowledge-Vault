d1c86f91a909643b759fa6a1681c0663b504c2c1
Dakpaneel <bradleyoosterveen@gmail.com>
2023-01-25 18:52:36 +0100

    Show campaign details

--- src/app/views/campaign-details-view/campaign-details-view.component.ts
+++ src/app/views/campaign-details-view/campaign-details-view.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Campaign } from 'src/app/models/campaign';
+import { CampaignService } from 'src/app/services/campaign.service';
 
 @Component({
   selector: 'app-campaign-details-view',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./campaign-details-view.component.scss']
 })
 export class CampaignDetailsViewComponent {
+  campaignId: number;
+  campaign: Campaign;
 
+  constructor(
+    private campaignService: CampaignService,
+    private route: ActivatedRoute
+  ) {
+    if (this.route.parent === null) {
+      this.campaignId = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      this.campaignId = Number(this.route.parent.snapshot.paramMap.get('id'));
+    }
+
+    this.campaign = this.campaignService.getCampaign(this.campaignId);
+  }
 }
