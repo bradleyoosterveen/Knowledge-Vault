6c46e89a0616f427a5585851bdd690dc16659ef0
Dakpaneel <bradleyoosterveen@gmail.com>
2023-02-01 16:05:56 +0100

    Add update for characters

--- src/app/views/character-view/character-view.component.ts
+++ src/app/views/character-view/character-view.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Character } from 'src/app/models/character';
 import { CharacterService } from 'src/app/services/character.service';
 
@@ -10,7 +12,12 @@ import { CharacterService } from 'src/app/services/character.service';
 })
 export class CharacterViewComponent {
   confirmCharacterDeletionModalOpen: boolean = false;
-  public character: Character;
+  character: Character;
+
+  form: FormGroup;
+  initialFormValues: any;
+
+  updatedSubscription: Subscription;
 
   constructor(
     private router: Router,
@@ -20,11 +27,58 @@ export class CharacterViewComponent {
     let id: number = Number(this.route.snapshot.paramMap.get('characterid'));
 
     this.character = this.characterService.getCharacter(id);
+
+    this.form = new FormGroup({
+      name: new FormControl(this.character.name, [
+        Validators.required
+      ]),
+      race: new FormControl(this.character.race),
+      sex: new FormControl(this.character.sex),
+      age: new FormControl(this.character.age),
+      size: new FormControl(this.character.size),
+      alignment: new FormControl(this.character.alignment),
+      notes: new FormControl(this.character.notes),
+    });
+
+    this.initialFormValues = this.form.value;
+
+    this.updatedSubscription = this.characterService.updated.subscribe(character => {
+      this.character = character;
+    })
   }
 
   onCharacterDeleteClick(): void {
     this.characterService.deleteCharacter(this.character);
 
     this.router.navigate(['campaign', Number(this.route.parent?.snapshot.paramMap.get('id')), 'characters']);
+  }
+
+  onResetToDefaultClick(): void {
+    this.form.reset(this.initialFormValues);
+  }
+
+  onSaveChangesClick(): void {
+    if (!this.form.valid)
+      return;
+
+    let charachter: Character = {
+      id: this.character.id,
+      campaignId: this.character.campaignId,
+      name: this.form.get('name')?.value || '',
+      race: this.form.get('race')?.value || '',
+      sex: this.form.get('sex')?.value || '',
+      age: this.form.get('age')?.value || '',
+      size: this.form.get('size')?.value || '',
+      alignment: this.form.get('alignment')?.value || '',
+    }
+
+    this.characterService.updateCharacter(charachter);
+
+    this.form.markAsPristine();
+    this.initialFormValues = this.form.value;
+  }
+
+  ngOnDestroy(): void {
+    this.updatedSubscription.unsubscribe();
   }
 }
